42be1400d5116a071c139586228a98437d3b8165
Xerol Wong <hlchbzz@gmail.com>
2025-02-14 03:07:06 +0900

    current time

--- AudioQueueTutorial/AudioQueuePlayer.swift
+++ AudioQueueTutorial/AudioQueuePlayer.swift
@@ -8,32 +8,37 @@
 
 import AudioToolbox
 import AVFoundation
+import Combine
 
 fileprivate func audioQueueOutputCallback(inUserData: UnsafeMutableRawPointer?, inQueue: AudioQueueRef, inBuffer: AudioQueueBufferRef) {
-    if let info = inUserData?.assumingMemoryBound(to: PlayerInfo.self) {
-        var bufferLength: UInt32 = info.pointee.bufferByteSize
-        var numPkgs: UInt32 = kNumberPackages
-        var status = AudioFileReadPacketData(info.pointee.mAudioFile!,
-                                             false,
-                                             &bufferLength,
-                                             info.pointee.mPacketDesc,
-                                             info.pointee.mCurrentPacket,
-                                             &numPkgs,
-                                             inBuffer.pointee.mAudioData)
-        if status == noErr {
-            inBuffer.pointee.mAudioDataByteSize = bufferLength
-        }
-        
-        status = AudioQueueEnqueueBuffer(info.pointee.mQueue!,
-                                         inBuffer,
-                                         numPkgs,
-                                         info.pointee.mPacketDesc)
-        info.pointee.mCurrentPacket += Int64(numPkgs)
-        
-        if numPkgs == 0 {
-            print("play finished")
-            AudioQueueStop(info.pointee.mQueue!, false)
-        }
+    guard let playerPointer = inUserData else {
+        return
+    }
+    let player = Unmanaged<AudioQueuePlayer>.fromOpaque(playerPointer).takeUnretainedValue()
+
+    var bufferLength: UInt32 = player.info.bufferByteSize
+    var numPkgs: UInt32 = kNumberPackages
+    var status = AudioFileReadPacketData(player.info.mAudioFile!,
+                                         false,
+                                         &bufferLength,
+                                         player.info.mPacketDesc,
+                                         player.info.mCurrentPacket,
+                                         &numPkgs,
+                                         inBuffer.pointee.mAudioData)
+    if status == noErr {
+        inBuffer.pointee.mAudioDataByteSize = bufferLength
+    }
+
+    status = AudioQueueEnqueueBuffer(inQueue,
+                                     inBuffer,
+                                     numPkgs,
+                                     player.info.mPacketDesc)
+    if status == noErr {
+        player.info.mCurrentPacket += Int64(numPkgs)
+    }
+
+    if numPkgs == 0 {
+        player.stop()
     }
 }
 
@@ -45,6 +50,8 @@ fileprivate struct PlayerInfo {
     var bufferByteSize: UInt32 = 0
     var mCurrentPacket: Int64 = 0
     var mPacketDesc: UnsafeMutablePointer<AudioStreamPacketDescription>?
+    var currentTime: AudioTimeStamp = AudioTimeStamp()
+    var duration: Float64 = 0
 }
 
 fileprivate let kNumberBuffers: UInt32 = 3
@@ -54,53 +61,57 @@ public class AudioQueuePlayer {
     // property
     fileprivate var audioFileUrl: URL
     fileprivate var info: PlayerInfo = PlayerInfo()
-    var playing: Bool = false
-    
+    private(set) public var playing: Bool = false
+    var duration: TimeInterval {
+        info.duration
+    }
+    private let currentTimeSubject: CurrentValueSubject<TimeInterval, Never> = .init(0)
+    var currentTimePublisher: any Publisher<TimeInterval, Never> {
+        currentTimeSubject.removeDuplicates {
+            abs($0 - $1) < 0.5
+        }
+    }
+    var cancellables = Set<AnyCancellable>()
+
     // life cycle
     init?(url: URL) {
         audioFileUrl = url
-        
-        do {
-            try AVAudioSession.sharedInstance().setActive(true)
-        } catch {
-            return nil
-        }
-        
+
         var audioFile: AudioFileID?
         if AudioFileOpenURL(url as CFURL, .readPermission, 0, &audioFile) == noErr {
             info.mAudioFile = audioFile
         } else {
             return nil
         }
-        
-        
+
         var descSize: UInt32 = UInt32(MemoryLayout<AudioStreamBasicDescription>.size)
         var dataFormat = AudioStreamBasicDescription()
-        if AudioFileGetProperty(audioFile!, kAudioFilePropertyDataFormat, &descSize, &dataFormat) == noErr {
-            info.mDataFormat = dataFormat
-        } else {
+        var status = AudioFileGetProperty(audioFile!, kAudioFilePropertyDataFormat, &descSize, &dataFormat)
+        guard status == noErr else {
             return nil
         }
-        
+        info.mDataFormat = dataFormat
+
+        var durationSize: UInt32 = UInt32(MemoryLayout<Float64>.size)
+        status = AudioFileGetProperty(audioFile!, kAudioFilePropertyEstimatedDuration, &durationSize, &info.duration)
+        guard status == noErr else {
+            return nil
+        }
+
         var queue: AudioQueueRef?
 
-        let result = withUnsafeMutablePointer(to: &info) { infoPointer in
-            if AudioQueueNewOutput(&dataFormat,
-                                   audioQueueOutputCallback,
-                                   infoPointer,
-                                   CFRunLoopGetCurrent(),
-                                   CFRunLoopMode.commonModes.rawValue,
-                                   0,
-                                   &queue) == noErr {
-                infoPointer.pointee.mQueue = queue
-                return true
-            } else {
-                return false
-            }
-        }
-        guard result else {
+        let pointerToSelf = Unmanaged.passUnretained(self).toOpaque()
+        status = AudioQueueNewOutput(&dataFormat,
+                                     audioQueueOutputCallback,
+                                     pointerToSelf,
+                                     CFRunLoopGetCurrent(),
+                                     CFRunLoopMode.commonModes.rawValue,
+                                     0,
+                                     &queue)
+        guard status == noErr else {
             return nil
         }
+        info.mQueue = queue
 
         var maxPacketSize: UInt32 = 0
         var propertySize: UInt32 = UInt32(MemoryLayout<UInt32>.size)
@@ -110,7 +121,7 @@ public class AudioQueuePlayer {
         } else {
             return nil
         }
-        
+
         var cookieSize: UInt32 = UInt32(MemoryLayout<UInt32>.size)
         if AudioFileGetPropertyInfo(audioFile!, kAudioFilePropertyMagicCookieData, &cookieSize, nil) == noErr {
             let magicCookie: UnsafeMutablePointer<CChar> = UnsafeMutablePointer<CChar>.allocate(capacity: Int(cookieSize))
@@ -118,39 +129,62 @@ public class AudioQueuePlayer {
             AudioQueueSetProperty(queue!, kAudioQueueProperty_MagicCookie, magicCookie, cookieSize)
             magicCookie.deallocate()
         }
-        
+
         info.mCurrentPacket = 0
-        
+
         for _ in 0..<kNumberBuffers {
             var buffer: AudioQueueBufferRef?
             if AudioQueueAllocateBuffer(queue!, info.bufferByteSize, &buffer) == noErr {
                 info.mBuffers.append(buffer!)
-                withUnsafeMutablePointer(to: &info) { infoPointer in
-                    audioQueueOutputCallback(inUserData: infoPointer, inQueue: queue!, inBuffer: buffer!)
-                }
+                audioQueueOutputCallback(inUserData: pointerToSelf, inQueue: queue!, inBuffer: buffer!)
             } else {
                 return nil
             }
         }
-        
+
         AudioQueueSetParameter(queue!, kAudioQueueParam_Volume, 1.0)
+
+        Timer.publish(every: 0.1, on: .current, in: .common).autoconnect().sink { [weak self] _ in
+            self?.updateCurrentTime()
+        }.store(in: &cancellables)
     }
-    
+
     deinit {
         if let audio = info.mAudioFile {
             AudioFileClose(audio)
             info.mAudioFile = nil
         }
-        
+
         if let queue = info.mQueue {
             AudioQueueDispose(queue, true)
             info.mQueue = nil
         }
-        
+
         if let desc = info.mPacketDesc {
             desc.deallocate()
             info.mPacketDesc = nil
         }
+    }
+
+    private func updateCurrentTime() {
+        guard let queue = info.mQueue else { return }
+        var timeStamp = AudioTimeStamp()
+        let status = AudioQueueGetCurrentTime(
+            queue,
+            nil,
+            &timeStamp,
+            nil
+        )
+        if status == noErr {
+            info.currentTime = timeStamp
+        }
+        currentTimeSubject.send(info.currentTime.mSampleTime / info.mDataFormat!.mSampleRate)
+    }
+
+    func stop() {
+        guard let queue = info.mQueue else { return }
+        AudioQueueStop(queue, true)
+        playing = false
     }
 }
 
@@ -160,7 +194,7 @@ extension AudioQueuePlayer {
         playing = true
         AudioQueueStart(info.mQueue!, nil)
     }
-    
+
     func pause() {
         playing = false
         AudioQueuePause(info.mQueue!)
--- AudioQueueTutorial/ViewController.swift
+++ AudioQueueTutorial/ViewController.swift
@@ -6,35 +6,66 @@
 //  Copyright © 2017 tomisacat. All rights reserved.
 //
 
+import AVFAudio
+import Combine
 import UIKit
 
 class ViewController: UIViewController {
-    
+    @IBOutlet weak var progressSlider: UISlider!
+    @IBOutlet var currentTimeLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
     var player: AudioQueuePlayer?
+    var cancellables = Set<AnyCancellable>()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        guard let path = Bundle.main.path(forResource: "guitar", ofType: "m4a") else {
+        setAudioSession()
+
+        guard let path = Bundle.main.path(forResource: "Avid", ofType: "flac") else {
             return
         }
-        
+
         let url = URL(fileURLWithPath: path)
-        player = AudioQueuePlayer(url: url)
+        guard let player = AudioQueuePlayer(url: url) else { return }
+        self.player = player
+
+        durationLabel.text = timeString(player.duration)
+        progressSlider.minimumValue = 0
+        progressSlider.maximumValue = Float(player.duration)
+        player.currentTimePublisher.sink { [weak self] currentTime in
+            guard let self else { return }
+            self.progressSlider.value = Float(currentTime)
+            self.currentTimeLabel.text = self.timeString(currentTime)
+        }.store(in: &cancellables)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func setAudioSession() {
+        let session = AVAudioSession.sharedInstance()
+        do {
+            try session.setCategory(.playback,
+                                    mode: .default,
+                                    policy: .longFormAudio
+            )
+            try session.setActive(true)
+        } catch {
+            print("Failed to set audio session category. Error: \(error.localizedDescription)")
+        }
     }
-    
+
+    private func timeString(_ time: TimeInterval) -> String {
+        let seconds = Int(time) % 60
+        let minutes = Int(time) / 60
+        return String(format: "%02d:%02d", minutes, seconds)
+    }
+
     @IBAction func playButtonClicked(sender: UIButton) {
         if player?.playing == true {
             player?.pause()
-            sender.setTitle("paused", for: .normal)
+            sender.setImage(.init(systemName: "play.fill"), for: .normal)
         } else {
             player?.play()
-            sender.setTitle("playing", for: .normal)
+            sender.setImage(.init(systemName: "pause.fill"), for: .normal)
         }
     }
 }
